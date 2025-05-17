98333ea20881aa5327cf5e6494ca0d07da6a46a8
luttfimohamad <luttfimohamad@gmail.com>
2025-05-17 17:53:08 +0700

    Add DELETE method for posts and update CORS headers

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   // Menambahkan header CORS
   // Diperlukan saat menggunakan backend terpisah (yang berjalan di domain berbeda)
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
@@ -38,6 +38,19 @@ app.post('/posts', async (req, res) => {
   const updatedPosts = [newPost, ...existingPosts];
   await storePosts(updatedPosts);
   res.status(201).json({ message: 'Stored new post.', post: newPost });
+});
+
+app.delete('/posts/:id', async (req, res) => {
+  const postId = req.params.id;
+  const existingPosts = await getStoredPosts();
+  const updatedPosts = existingPosts.filter((post) => post.id !== postId);
+
+  if (updatedPosts.length === existingPosts.length) {
+    return res.status(404).json({ message: 'Post not found.' });
+  }
+
+  await storePosts(updatedPosts);
+  res.status(200).json({ message: 'Post deleted.' });
 });
 
 app.listen(8080);
